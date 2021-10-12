d1c5ef9fa5740cb2d712d3d777abd0a61b2f0eb6
yogadarma182 <yogadarma182@gmail.com>
2021-10-12 14:47:40 +0800

    update script

--- node_exporter.sh
+++ node_exporter.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 wget https://github.com/prometheus/node_exporter/releases/download/v1.2.2/node_exporter-1.2.2.linux-amd64.tar.gz
 tar -xf node_exporter-1.2.2.linux-amd64.tar.gz
+sudo mv node_exporter-1.2.2.linux-amd64/node_exporter /usr/local/bin
 rm -r node_exporter-1.2.2.linux-amd64*
 sudo useradd -rs /bin/false node_exporter
 touch /etc/systemd/system/node_exporter.service
